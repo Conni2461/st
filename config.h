f1b17d129c75debb2090505e654396ec50335ed5
Simon Hauser <Simon-Hauser@outlook.de>
2020-12-09 17:42:43 +0100

    Style font. Only light and regular

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "monospace:pixelsize=17:antialias=true:autohint=true";
+static char *font = "monospace:style=Light,Regular:pixelsize=17:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
 	"JoyPixels:pixelsize=15:antialias=true:autohint=true"
