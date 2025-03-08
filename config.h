04209178ce06e5d12dc506b0d8faf52f225ae74f
Simon Hauser <Simon-Hauser@outlook.de>
2025-03-08 17:21:32 +0100

    feat: apply font2 patch
    patch: https://st.suckless.org/patches/font2/st-font2-0.8.5.diff

--- config.h
+++ config.h
@@ -6,6 +6,10 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "monospace:style=Light,Regular:pixelsize=15:antialias=true:autohint=true";
+static char *font2[] = {
+	"JoyPixels:pixelsize=13:antialias=true:autohint=true"
+};
+
 static int borderpx = 2;
 
 /*
